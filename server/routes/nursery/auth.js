636767abd810feefa46d62bf72f7d18cefd80ad3
Inam Ul Huq <inambe.dev@gmail.com>
2020-12-29 03:36:23 +0500

    server:token generation success

--- server/routes/nursery/auth.js
+++ server/routes/nursery/auth.js
@@ -1,9 +1,99 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const jsonwebtoken = require("jsonwebtoken");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-	res.render("index", { title: "Express" });
-});
+const Nursery = require("../../models/mongoose/nursery");
+const { hash, verify } = require("../../models/password");
+
+const router = express.Router();
+
+router.get(
+	"/sign-up",
+	body("name").notEmpty(),
+	body("location").notEmpty(),
+	body("email").isEmail(),
+	body("password").isLength({ min: 5 }),
+	async (req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty())
+			return res.json({
+				success: false,
+				message: "Information is not valid",
+				data: errors.array(),
+			});
+
+		const { name, email, password, location } = req.body;
+		const nursery = new Nursery({
+			name,
+			email,
+			password: await hash(password),
+			location,
+		});
+		try {
+			await nursery.save();
+			return res.json({
+				success: true,
+			});
+		} catch (e) {
+			return res.json({
+				success: false,
+				message: e.message,
+			});
+		}
+	}
+);
+
+router.get(
+	"/sign-in",
+	body("email").isEmail(),
+	body("password").isLength({ min: 5 }),
+	async (req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty())
+			return res.json({
+				success: false,
+				message: "Information is not valid",
+				data: errors.array(),
+			});
+
+		const { email, password } = req.body;
+
+		const nursery = await Nursery.findOne({ email: email });
+		if (!nursery)
+			return res.json({
+				success: false,
+				message: "No Nursery was found with this email",
+			});
+
+		const passwordGood = await verify(password, nursery.password);
+		if (!passwordGood)
+			return res.json({
+				success: false,
+				message: "Password is wrong",
+			});
+
+		try {
+			const token = jsonwebtoken.sign(
+				{
+					userType: "nursery",
+					sub: nursery._id,
+				},
+				process.env.SECRET,
+				{
+					expiresIn: "30 days",
+				}
+			);
+			return res.json({
+				success: true,
+				data: token,
+			});
+		} catch (e) {
+			return res.json({
+				success: false,
+				message: e.message,
+			});
+		}
+	}
+);
 
 module.exports = router;
