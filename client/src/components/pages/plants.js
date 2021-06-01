70673eb1ea27a0b38cd26838808ea8471495eaed
Inam Ul Huq <inambe.dev@gmail.com>
2021-06-02 00:28:27 +0500

    add pagination

--- client/src/components/pages/plants.js
+++ client/src/components/pages/plants.js
@@ -1,34 +1,78 @@
 import React, { useEffect, useState } from "react";
-import { Row } from "react-bootstrap";
+import { Col, Pagination, Row } from "react-bootstrap";
+import { useLocation } from "react-router";
 import GGClient from "../../api/GGClient";
 import Loading from "../loading";
 import ProductCard from "../productCard";
 
 function Plants() {
 	const [products, setProducts] = useState();
+	const [paging, setPaging] = useState({});
+	const location = useLocation();
 
 	useEffect(() => {
 		(async () => {
-			const { data } = await GGClient.get("/products/list");
+			const { data } = await GGClient.get(
+				"/products/list" + location.search
+			);
 			if (data.success) {
 				setProducts(data.data);
+				setPaging({
+					next: data.next,
+					prev: data.prev,
+				});
 			}
 		})();
-	}, []);
+	}, [location]);
+
 	return (
 		<div>
 			<h1 className="display-5 mb-3">Products</h1>
-			<Row>
+			<Row className="mb-3">
 				{products ? (
-					products.map((product, i) => (
-						<ProductCard key={i} product={product} />
-					))
+					products.length > 0 ? (
+						products.map((product, i) => (
+							<ProductCard key={i} product={product} />
+						))
+					) : (
+						<Col>
+							<p className="text-center">No product was found.</p>
+						</Col>
+					)
 				) : (
 					<Loading />
 				)}
 			</Row>
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+				}}
+			>
+				<Pagination>
+					<Pagination.Prev
+						href={getPageHref(paging.prev)}
+						disabled={!paging.prev}
+					/>
+					<Pagination.Next
+						href={getPageHref(paging.next)}
+						disabled={!paging.next}
+					/>
+				</Pagination>
+			</div>
 		</div>
 	);
 }
+
+const getPageHref = (num) => {
+	if (!num) return "#";
+
+	let href = window.location.href;
+	href = href.replace(/page=[0-9]/, "page=" + num);
+
+	if (href.includes("page=")) return href;
+
+	return href + "?page=" + num;
+};
 
 export default Plants;
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const Product = require("../models/mongoose/product");
 const Nursery = require("../models/mongoose/nursery");
+const { PAGE_LIMIT } = require("../constants");
 
 const router = express.Router();
 
 router.get("/list", async (req, res) => {
 	try {
-		const limit = parseInt(req.query.limit);
+		const page = parseInt(req.query.page) || 1;
+		const limit = parseInt(req.query.limit) || PAGE_LIMIT;
+		const skip = (page - 1) * limit;
 
 		const products = await Product.find()
 			.sort({ "createdAt": "desc" })
 			.limit(limit)
+			.skip(skip)
 			.populate("nursery")
 			.exec();
 
-		return res.json({ success: true, data: products });
+		let next;
+		let prev;
+		const count = await Product.find().skip(skip).countDocuments();
+		if (count > limit) {
+			next = page + 1;
+		}
+		if (page > 1) {
+			prev = page - 1;
+		}
+
+		return res.json({ success: true, data: products, next, prev });
 	} catch (e) {
 		return res.send({ success: false, message: e.message });
 	}
